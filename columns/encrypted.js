ddf2bd99e9a1fb2f5062fa41c55665e0509e297d
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-06-12 17:21:28 -0700

    fix lint

--- columns/encrypted.js
+++ columns/encrypted.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'
+
+const Iron = require('@hapi/iron')
 
 const orm = require('..')
-const Iron = require('@hapi/iron');
 
+module.exports = encrypted
 
-module.exports = function(schema, cryptoOpts) {
-  cryptoOpts.iron = cryptoOpts.iron
-    ? { ...Iron.defaults, ...cryptoOpts.iron }
-    : { ...Iron.defaults };
+function encrypted (schema, { password, iron = Iron.defaults }) {
+  iron = { ...Iron.defaults, ...(iron || Iron.defaults) }
 
   return orm.col(schema, {
     encode (appData) {
-      return Iron.seal(appData, cryptoOpts.password, cryptoOpts.iron)
+      return Iron.seal(appData, password, iron)
     },
     decode (dbData) {
-      return Iron.unseal(dbData, cryptoOpts.password, cryptoOpts.iron)
+      return Iron.unseal(dbData, password, iron)
     },
     encodeQuery (appData) {
-      return Iron.seal(appData, cryptoOpts.password, cryptoOpts.iron)
+      return Iron.seal(appData, password, iron)
     }
-  });
+  })
 }
