ae28e7711f9da15687abe97467f0465c99d1fce0
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-12-09 14:21:36 -0800

    remove pgtools to fix tests

--- test/db.js
+++ test/db.js
@@ -1,16 +1,12 @@
 'use strict'
 
 module.exports = {
-  createdb: createdb,
   setup: setup,
   getConnection: getConnection
 }
 
-const fs = require('fs')
 const ormnomnom = require('..')
-const path = require('path')
 const pg = require('pg')
-const pgtools = require('pgtools')
 
 const TEST_DB_NAME = process.env.TEST_DB_NAME || 'onn_test'
 const client = new pg.Client({
@@ -18,45 +14,26 @@ const client = new pg.Client({
 })
 let connected = false
 
-function createdb () {
-  ormnomnom.setConnection(getConnection)
-  return pgtools.dropdb({}, TEST_DB_NAME).catch(err => {
-    // ignore this error since it throws if the db doesn't exist
-    if (err.name !== 'invalid_catalog_name') {
-      throw err
-    }
-  }).then(() => {
-    return pgtools.createdb({}, TEST_DB_NAME)
-  }).then(() => {
-    return getConnection().then(client => {
-      return client.query(fs.readFileSync(path.join(__dirname, 'fixture.sql'), { encoding: 'utf8' })).then(() => client.end())
-    })
-  })
-}
-
 function setup (beforeEach, afterEach, teardown) {
-  beforeEach(function () {
-    return getConnection().then(client => {
-      return client.query('BEGIN')
-    })
+  beforeEach(async () => {
+    const client = await getConnection()
+    return client.query('BEGIN')
   })
 
-  afterEach(function () {
-    return getConnection().then(client => {
-      return client.query('ROLLBACK')
-    })
+  afterEach(async () => {
+    const client = await getConnection()
+    return client.query('ROLLBACK')
   })
 
-  teardown(function () {
-    return getConnection().then(client => {
-      return client.end()
-    })
+  teardown(async () => {
+    const client = await getConnection()
+    return client.end()
   })
 
   ormnomnom.setConnection(getConnection)
 }
 
-async function getConnection (commit) {
+async function getConnection () {
   if (connected) {
     return client
   }
