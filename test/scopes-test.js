2bd95a073de46ace2bc8ef7bb68e4913c4f263b3
Hayden Faulds <fauldsh@gmail.com>
2022-03-15 12:15:09 +0000

    Allow configuration of base queryset class
    In `orm(Table, { ddl }, options)`, add `options.querySetClass` to allow for
    chained, custom queries.
    
    This is roughly equivalent to Django's instructions on how to modify
    the default QuerySet [1] or Rail's ActiveRecord scopes [2].
    
    [1]: https://docs.djangoproject.com/en/4.0/topics/db/managers/#modifying-a-manager-s-initial-queryset
    [2]: https://www.rubyguides.com/2019/10/scopes-in-ruby-on-rails/

---
+++ test/scopes-test.js
@@ -0,0 +1,108 @@
+'use strict'
+
+const { beforeEach, afterEach, teardown, test } = require('tap')
+
+const orm = require('..')
+const db = require('./db')
+
+const QuerySet = require('../lib/queryset.js')
+
+db.setup(beforeEach, afterEach, teardown)
+
+test('test calling a scope', assert => {
+  class Node {
+    constructor (obj) {
+      this.id = obj.id
+      this.name = obj.name
+      this.val = obj.val
+    }
+  }
+
+  class NotAQuerySet { }
+
+  assert.throws(() => {
+    orm(Node, {
+      id: { type: 'integer' },
+      name: { anyOf: [{ type: 'null' }, { type: 'string' }], default: null },
+      val: { type: 'number' }
+    }, {
+      querySetClass: NotAQuerySet
+    })
+  })
+
+  assert.end()
+})
+
+test('test calling a scope', assert => {
+  class Node {
+    constructor (obj) {
+      this.id = obj.id
+      this.name = obj.name
+      this.val = obj.val
+    }
+  }
+
+  class NodeQuerySet extends QuerySet {
+    name (n) {
+      return this.filter({ name: n })
+    }
+  }
+
+  const NodeObjects = orm(Node, {
+    id: { type: 'integer' },
+    name: { anyOf: [{ type: 'null' }, { type: 'string' }], default: null },
+    val: { type: 'number' }
+  }, {
+    querySetClass: NodeQuerySet
+  })
+
+  return NodeObjects.create({
+    name: 'jake busey',
+    val: -100
+  }).then(() => {
+    return NodeObjects.all().name('jake busey').then(xs => {
+      assert.equal(xs.length, 1)
+      assert.equal(xs[0].name, 'jake busey')
+      assert.equal(xs[0].val, -100)
+    })
+  })
+})
+
+test('test chaining scopes', assert => {
+  class Node {
+    constructor (obj) {
+      this.id = obj.id
+      this.name = obj.name
+      this.val = obj.val
+    }
+  }
+
+  class NodeQuerySet extends QuerySet {
+    name (n) {
+      return this.filter({ name: n })
+    }
+
+    val (v) {
+      return this.filter({ val: v })
+    }
+  }
+
+  const NodeObjects = orm(Node, {
+    id: { type: 'integer' },
+    name: { anyOf: [{ type: 'null' }, { type: 'string' }], default: null },
+    val: { type: 'number' }
+  }, {
+    querySetClass: NodeQuerySet
+  })
+
+  return NodeObjects.create({
+    name: 'jake busey',
+    val: -100
+  }).then(() => {
+    return NodeObjects.all().name('jake busey').val(-100).then(xs => {
+      assert.equal(xs.length, 1)
+      assert.equal(xs[0].name, 'jake busey')
+      assert.equal(xs[0].val, -100)
+    })
+  })
+})
